6846c9e6d141b07d16fc439180bfed0841d283a1
rochefort <terasawan@gmail.com>
2015-11-03 22:52:56 +0900

    Fix hash signs for ruby1.9 style

--- lib/terminal/table/cell.rb
+++ lib/terminal/table/cell.rb
@@ -1,13 +1,11 @@
 module Terminal
   class Table
     class Cell
-
       def align(val, position, length)
         # positions = { :left => :ljust, :right => :rjust, :center => :center }
-        positions = { :left => :mb_ljust, :right => :mb_rjust, :center => :mb_center }
+        positions = { left: :mb_ljust, right: :mb_rjust, center: :mb_center }
         val.public_send(positions[position], length)
       end
-
     end
   end
 end
