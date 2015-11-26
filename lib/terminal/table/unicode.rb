6be7c8fbadcc68262e4b6b47a0edac69a855f423
rochefort <terasawan@gmail.com>
2015-11-27 02:12:43 +0900

    Fix leaking require settings

--- lib/terminal/table/unicode.rb
+++ lib/terminal/table/unicode.rb
@@ -1,7 +1,7 @@
 require 'unicode/display_width'
+require 'mb_string'
 require 'terminal-table'
 
 require 'terminal/table/cell'
 require 'terminal/table/table'
-require 'terminal/table/unicode/ext/string'
 require 'terminal/table/unicode/version'
